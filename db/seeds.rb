e1bbb3c2b96cc8b0f39aecc77910cc10ae1d121d
Lydia Gregory <ljg2gb@virginia.edu>
2020-05-29 21:41:50 -0600

    destroys seeds in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-
+Point.destroy_all
+Story.destroy_all
+User.destroy_all
